439291600d29084b3932481ff2d26eff32e52c59
Jeffrey LaBau <j.t.labau@gmail.com>
2026-04-09 21:31:05 -0600

    Align with jellyfin-plugin-template: add .editorconfig, jellyfin.ruleset, Directory.Build.props, GitHub workflows (build/test/publish/codeql), renovate.json. Add explicit Guid override in Plugin.cs. Disable strict CA rules for existing codebase. Bump to v1.1.0.0.

--- Jellyfin.Plugin.Audiobookshelf/Plugin.cs
+++ Jellyfin.Plugin.Audiobookshelf/Plugin.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
@@ -17,18 +18,6 @@ namespace Jellyfin.Plugin.Audiobookshelf;
 public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
 {
     /// <summary>
-    /// Gets the singleton instance of this plugin, set during construction.
-    /// </summary>
-    public static Plugin? Instance { get; private set; }
-
-    /// <inheritdoc />
-    public override string Name => "Audiobookshelf";
-
-    /// <inheritdoc />
-    public override string Description =>
-        "Browse and play audiobooks from Audiobookshelf with bidirectional progress sync.";
-
-    /// <summary>
     /// Initializes a new instance of the <see cref="Plugin"/> class.
     /// </summary>
     /// <param name="applicationPaths">Jellyfin application path provider.</param>
@@ -40,15 +29,30 @@ public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     }
 
     /// <inheritdoc />
+    public override string Name => "Audiobookshelf";
+
+    /// <inheritdoc />
+    public override Guid Id => Guid.Parse("7b2f4a1d-e3c0-4e8b-9d5a-3f7c6b2e0a14");
+
+    /// <inheritdoc />
+    public override string Description =>
+        "Browse and play audiobooks from Audiobookshelf with bidirectional progress sync.";
+
+    /// <summary>
+    /// Gets the singleton instance of this plugin, set during construction.
+    /// </summary>
+    public static Plugin? Instance { get; private set; }
+
+    /// <inheritdoc />
     public IEnumerable<PluginPageInfo> GetPages()
     {
         return new[]
         {
             new PluginPageInfo
             {
-                Name = "Audiobookshelf",
+                Name = Name,
                 DisplayName = "Audiobookshelf",
-                EmbeddedResourcePath = $"{GetType().Namespace}.Configuration.configPage.html"
+                EmbeddedResourcePath = string.Format(CultureInfo.InvariantCulture, "{0}.Configuration.configPage.html", GetType().Namespace)
             }
         };
     }
